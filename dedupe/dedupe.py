f9d032795f93220098d8987a5772c64cc570d7c7
clee <clee@tuta.io>
2021-04-14 18:41:35 +0000

    add index param

--- dedupe/dedupe.py
+++ dedupe/dedupe.py
@@ -35,19 +35,19 @@ def populate_dict_of_duplicate_docs(hit):
 
 # Loop over all documents in the index, and populate the
 # dict_of_duplicate_docs data structure.
-def scroll_over_all_docs():
-    for hit in helpers.scan(es, index='docker-compose'):
+def scroll_over_all_docs(_index):
+    for hit in helpers.scan(es, index=_index):
         populate_dict_of_duplicate_docs(hit)
 
 
-def loop_over_hashes_and_remove_duplicates():
+def loop_over_hashes_and_remove_duplicates(_index):
     # Search through the hash of doc values to see if any
     # duplicate hashes have been found
     for hashval, array_of_ids in dict_of_duplicate_docs.items():
       if len(array_of_ids) > 1:
         #print("********** Duplicate docs hash=%s **********" % hashval)
         # Get the documents that have mapped to the current hasval
-        matching_docs = es.mget(index="docker-compose", doc_type="doc", body={"ids": array_of_ids})
+        matching_docs = es.mget(index=_index, doc_type="doc", body={"ids": array_of_ids})
         for doc in matching_docs['docs']:
             # In order to remove the possibility of hash collisions,
             # write code here to check all fields in the docs to
@@ -55,14 +55,14 @@ def loop_over_hashes_and_remove_duplicates():
             # DELETE operation on all except one.
             # In this example, we just print the docs.
             print("doc=%s\n" % doc)
-            #es.delete(index="docker-compose",doc_type="doc",id=doc._id)
+            #es.delete(index=_index,doc_type="doc",id=doc._id)
             break
 
 
 
-def main():
-    scroll_over_all_docs()
-    loop_over_hashes_and_remove_duplicates()
+def main(_index):
+    scroll_over_all_docs(_index)
+    loop_over_hashes_and_remove_duplicates(_index)
 
 
-main()
+main('docker-compose')
